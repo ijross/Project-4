abcc93e2ff59e1f6780e2fa8b20313ed960125a6
Jesse Cheun <jcheun@ucsc.edu>
2014-06-05 05:00:26 -0700

    Allocate Triple Indirect Pointer

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ cp Makefile.inc /usr/src/lib/libc/posix
 cp lib.h /usr/src/include
 cp callnr.h /usr/src/include/minix
 cp read.c /usr/src/servers/vfs
+cp write.c /usr/src/servers/vfs
 
 cd /usr/src/servers
 make install
-
 
 cd /usr/src/tools
 make install
